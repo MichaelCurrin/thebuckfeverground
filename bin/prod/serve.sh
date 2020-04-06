ceed32ace0dee197cdff867aeef7dc7b039ff1cb
Michael Currin <michael.currin@gmail.com>
2020-04-06 21:14:51 +0200

    chore: Delete files

--- bin/prod/serve.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-# Serve prod site locally.
-# A Python server is used as it behaves more like a remote web server than using Jekyll.
-# Assumes the site has been built already.
-
-cd _site_prod
-python3 -m http.server 8002
